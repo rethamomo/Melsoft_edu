451feda313665d31ba912f69aba19937e772ef30
Mosa Moime <mmoime023@student.wethinkcode.co.za>
2024-09-27 09:36:54 +0200

    dashboard page added

--- main.py
+++ main.py
@@ -36,6 +36,10 @@ def signup():
         return redirect(url_for('login'))
     return render_template('signup.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
