aec8eb5b5d8286ab05011bbb442945e7ad5f3744
Tshegofatso Sekgothe <tsgsekgothe6969@gmail.com>
2024-09-27 11:36:21 +0200

    addded greeting text

--- API.py
+++ API.py
@@ -4,9 +4,12 @@ import os
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
-response = model.generate_content("Do not give me direct answers just push me in the right direction")
-print(response.text)
+
+def greet_user():
+    greeting = model.generate_content("Do not give me direct answers just push me in the right direction")
+    return greeting
 
 def make_request(subject, proficiency, question):
+    
     response = model.generate_content("explain this " + subject + " to me at this proficiency " + proficiency + ": " + question)
     return response.text
